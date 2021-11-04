ab728b2d36b5b1ced66021c1a5cf31653b8da752
hongho092 <93205420+hongho092@users.noreply.github.com>
2021-11-04 22:36:31 +0700

    ss7

---
+++ ss7/thực hành/sudungtoantu.js
@@ -0,0 +1,6 @@
+let chieurong = parseInt(prompt('Hay nhap chieu rong'));
+let chieudai = parseInt(prompt('Hay nhap chieu dai'));
+let S= chieurong * chieudai;
+document.write('Chieu rong la:' + chieurong);
+document.write('Chieu dai la:' + chieudai);
+document.write('Dien tich la:' + S);
